89fe1e212a8143829100f54b6175ada4d6d6ab8e
avinasdube <avidubey712@gamil.com>
2024-06-16 23:28:30 +0530

    [new] : created new login component

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -1,8 +1,22 @@
 import React from "react";
+import '../../styles/form.scss';
+import Logo from "../../components/Logo/Logo";
 
 const Login = () => {
     return (
-        <div>Login Page</div>
+        <div className="form-container">
+            <form>
+                <div className="form-head">
+                    <Logo />
+                    <div className="form-heading">
+                        Login to collabordraw
+                    </div>
+                </div>
+                <input type="email" placeholder="Enter your email id"></input>
+                <input type="password" placeholder="Enter password"></input>
+                <button>Login</button>
+            </form>
+        </div>
     )
 }
 
