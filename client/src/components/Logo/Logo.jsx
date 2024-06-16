4c8780e7e57b23ea2a0a4848552a58a5a9958750
avinasdube <avidubey712@gamil.com>
2024-06-16 23:27:53 +0530

    [new] : created new logo component

--- client/src/components/Logo/Logo.jsx
+++ client/src/components/Logo/Logo.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import './Logo.scss';
 
+import logoImg from '../../assets/icons/collabordrawicon.png';
+
 const Logo = () => {
 
     return (
-        <div>
-
+        <div className="logo-container">
+            <img src={logoImg} alt="" className="logo-icon"></img>
         </div>
     )
 }
