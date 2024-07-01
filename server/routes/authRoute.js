6a48a8ce2667b14b5862ffc0a2db662174694f16
avinasdube <avidubey712@gamil.com>
2024-07-01 19:32:34 +0530

    [getuser-route] : new route to get user details with authorization

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -1,6 +1,7 @@
 // importing dependencies and controllers
 import express from "express";
-import { login, signup } from "../controllers/authController.js";
+import { getLoggedInUserDetails, login, signup } from "../controllers/authController.js";
+import { verifyJWT } from '../middlewares/authMiddleware.js';
 
 // defining router
 const router = express.Router();
@@ -8,5 +9,6 @@ const router = express.Router();
 // setting routes
 router.post("/signup", signup);
 router.post("/login", login);
+router.get("/userdetails", verifyJWT, getLoggedInUserDetails);
 
 export default router;
